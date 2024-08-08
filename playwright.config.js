62bea30e23d973be44e777436a10decc36585b03
suh1z <jobo.holmberg@gmail.com>
2024-08-08 10:47:34 +0300

    Script

--- playwright.config.js
+++ playwright.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:5000/',
+    baseURL: 'http://localhost:8080',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -70,7 +70,7 @@ export default defineConfig({
 
   webServer: {
     command: 'npm run start',
-    url: 'http://localhost:5000/',
+    url: 'http://localhost:8080/',
     reuseExistingServer: !process.env.CI,
   },
 
